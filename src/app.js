3ef001c5eec725fefac1807fe56a04f3e92dfd36
root <root@BETELNOTE658.beteltecnologia.net.br>
2022-10-14 00:30:30 -0300

    refatoracao livros

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express"
 import db from "./config/database.js";
+// import livros from "./models/Livro.js"
+import routes from "./routes/index.js"
+
 
 db.on("error", console.log.bind(console, 'Erro de conexão:'));
 db.once("open", () => {
@@ -9,47 +12,32 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(express.json());
+routes(app);
 
-const livros = [
-    {
-        id: 1,
-        "titulo": "Senhor dos aneis"
-    },
-    {
-        id: 2,
-        "titulo": "O Hobbit"
-    }
-]
 
-app.get('/', (req, res) => {
-    res.status(200).send('Pagina inicial');
-})
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
-})
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com sucesso')
-})
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-})
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro removido com sucesso!`);
-})
+// app.get('/livros/:id', (req, res) => {
+//     livros.buscaLivro((err, livros) => {
+//         res.json(livros[livro]);
+//     });
+// })
+// app.post('/livros', (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send('Livro cadastrado com sucesso')
+// })
+// app.put('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo;
+//     res.json(livros);
+// })
+// app.delete('/livros/:id', (req, res) => {
+//     let {id} = req.params
+//     let index = buscaLivro(id);
+//     livros.splice(index, 1);
+//     res.send(`Livro removido com sucesso!`);
+// })
 
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
+// function buscaLivro(id){
+//     return livros.findIndex(id)
+// }
 
 export default app
